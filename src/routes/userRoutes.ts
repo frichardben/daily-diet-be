34e3c355146c71fc30092190ab5fc896215d2de9
Richard Ben <frichardben@yahoo.com.br>
2024-02-23 21:12:45 -0300

    feat(route): added middlewares

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -2,11 +2,12 @@ import { FastifyInstance } from 'fastify';
 
 import { CreateUserController } from '../modules/users/useCase/createUser/createUserController';
 import { GetAllUsersController } from '../modules/users/useCase/getAllUsers/getAllUsersController';
+import { checkSessionIdExists } from '../infra/middlewares/check-session-id-exists';
 
 export async function userRoutes(app: FastifyInstance) {
   const createUseController = new CreateUserController();
   const getAllUsersController = new GetAllUsersController();
 
   app.post('/', createUseController.handle);
-  app.get('/', getAllUsersController.handle);
+  app.get('/', { preHandler: checkSessionIdExists }, getAllUsersController.handle);
 }
