4ee4b9f87d5cc9977e65a61da673463cb376371b
Richard Ben <frichardben@yahoo.com.br>
2024-02-23 21:13:10 -0300

    chore: added prefix meals

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,14 @@ import cookies from '@fastify/cookie';
 import fastify from 'fastify';
 import { userRoutes } from './routes/userRoutes';
 import { env } from './infra/env';
+import { mealRoutes } from './routes/mealRoutes';
 
 const app = fastify();
 
 app.register(cookies);
 
 app.register(userRoutes, { prefix: 'users' });
+app.register(mealRoutes, { prefix: 'meals' });
 
 app.listen({ port: env.PORT }).then(() => {
   console.log(`Server listening on port ${env.PORT}`);
