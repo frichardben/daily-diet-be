802fb670bb2f8d26db76a689163a8464aa75d0d3
Richard Ben <frichardben@yahoo.com.br>
2024-02-25 11:19:41 -0300

    feat(routes): added  mealId

--- src/routes/mealRoutes.ts
+++ src/routes/mealRoutes.ts
@@ -2,13 +2,22 @@ import { FastifyInstance } from 'fastify';
 import { CreateMealController } from '../modules/meals/useCase/createMeal/createMealController';
 import { GetAllMealsController } from '../modules/meals/useCase/getAllMeals/getAllMealsController';
 import { checkSessionIdExists } from '../infra/middlewares/check-session-id-exists';
-
-
+import { EditMealByIdController } from '../modules/meals/useCase/editMealById/editMealByIdController';
 
 export async function mealRoutes(app: FastifyInstance) {
   const createUseController = new CreateMealController();
   const getAllUsersController = new GetAllMealsController();
+  const editMealByIdController = new EditMealByIdController();
 
   app.post('/', createUseController.handle);
-  app.get('/', { preHandler: checkSessionIdExists }, getAllUsersController.handle);
+  app.get(
+    '/',
+    { preHandler: checkSessionIdExists },
+    getAllUsersController.handle
+  );
+  app.put(
+    '/:mealId',
+    { preHandler: checkSessionIdExists },
+    editMealByIdController.handle
+  );
 }
