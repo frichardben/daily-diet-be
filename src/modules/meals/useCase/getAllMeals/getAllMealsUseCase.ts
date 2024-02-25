9992fb97f176a1a9e4aa5301e0ac25839f206924
Richard Ben <frichardben@yahoo.com.br>
2024-02-25 11:18:58 -0300

    feat: search order by desc

--- src/modules/meals/useCase/getAllMeals/getAllMealsUseCase.ts
+++ src/modules/meals/useCase/getAllMeals/getAllMealsUseCase.ts
@@ -2,7 +2,9 @@ import { knex } from '../../../../database';
 
 export class GetAllMealsUseCase {
   async execute(sessionId) {
-    const meals = await knex('meals').where({ user_id: sessionId });
+    const meals = await knex('meals')
+      .where({ user_id: sessionId })
+      .orderBy('date', 'desc');
 
     return {
       meals,
